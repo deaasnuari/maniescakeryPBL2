e0290336e9c77fb4a59d40de5d62d3a36a3df8b0
cell-stack <cello.marcelino2735@gmail.com>
2025-04-28 00:29:11 +0700

    add struktur folder

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Homepage')
+@section('content')
+    <div class="container mx-auto flex flex-col items-center justify-center h-screen">
+        <div class="flex flex-col items-center justify-center gap-5">
+            <h1 class="text-4xl font-bold">Welcome to Manies Cakery</h1>
+            <p class="text-lg">Delicious cakes made with love.</p>
+            <a href="#" class="bg-[#DFAC6B] text-white py-2 px-4 rounded">Contoh page aja gaiss</a>
+        </div>  
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Manies Cakery')</title>
+    <link rel="stylesheet" href="css/flowbite.min.css">
+    <script src="js/flowbite.min.js"></script>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-[#F4EDE1] ">
+    <header class="bg-[#493C32] flex items-center py-4 px-10 justify-between">
+        <img src="assets/maniescakery2.png" alt="" class="w-40">
+        <div class="text-white flex gap-15">
+            <a href="" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">Home</a>
+            <a href="" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">Product</a>
+            <a href="" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">About Us</a>
+            <a href="" class="py-1 px-4 hover:text-[#DFAC6B] duration-100">Login</a>
+        </div>
+    </header>
+    <main class="min-h-screen mb-46">
+        @yield('content')
+    </main>
+    <footer class="flex flex-col">
+        <div class="bg-[#DFAC6B] flex items-center justify-between px-10 relative h-30 text-white font-bold">
+            <div class="flex items-center gap-20">
+                <p class="text-2xl"> follow us on:</p>
+                <div class="flex gap-5 items-center">
+                    <a href=""><img class="w-8" src="assets/icons/wa.png" alt=""></a>
+                    <a href=""><img class="w-8" src="assets/icons/gojek.icon.png" alt=""></a>
+                    <a href=""><img class="w-8" src="assets/icons/instagram.png" alt=""></a>
+                </div>
+            </div>
+            <div class="absolute top-[-12%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                <img src="assets/PI.png" alt="">
+            </div>
+            <div class="text-2xl text-center text-bold">
+                <p>Made with Love, <br>Enjoyed with Happiness</p>
+            </div>
+        </div>
+        <div class="bg-[#493C32] text-white flex justify-center py-5">
+            <p>copyright © 2023 Manies Cakery</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
