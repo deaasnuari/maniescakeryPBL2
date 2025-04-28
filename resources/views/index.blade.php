4d8c3ec755588984b43899a4ce566f73144303c0
cell-stack <cello.marcelino2735@gmail.com>
2025-04-28 20:17:53 +0700

    add empty confirmpopup and login blade templates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,6 @@
             <h1 class="text-4xl font-bold">Welcome to Manies Cakery</h1>
             <p class="text-lg">Delicious cakes made with love.</p>
             <a href="#" class="bg-[#DFAC6B] text-white py-2 px-4 rounded">Contoh page aja gaiss</a>
-        </div>  
+        </div>
     </div>
 @endsection
