84bda7611afe60a153400fee2ea2dcc25d520ae8
Ndan00 <hamdanthedk@gmail.com>
2025-04-28 22:04:06 +0700

    ni ya we

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::get('pages/landingadmin', function () {
+    return view('pages/landingadmin');
+});
+
+
+Route::get('pages/profil', function () {
+    return view('pages/profil');
+});
