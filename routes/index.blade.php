cde4b2f7d73f7ecd338dcf39507c8ec2f9492f47
Ndan00 <hamdanthedk@gmail.com>
2025-05-03 16:31:50 +0700

    asets gambar

--- routes/index.blade.php
+++ routes/index.blade.php
@@ -79,7 +79,7 @@
 
 
 
-{{-- <div class="max-w-6xl mx-auto px-4 py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+<div class="max-w-6xl mx-auto px-4 py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
   <!-- Item Start -->
   @foreach ([
     ['title' => 'Cake', 'image' => 'Cake-M.png'],
@@ -110,7 +110,7 @@
     </a>
   </div>
   @endforeach
-</div> --}}
+</div>
 
 
 
