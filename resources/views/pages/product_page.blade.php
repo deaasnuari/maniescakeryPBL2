6a5b6b4e598d62765b24758c290006b055f6c89f
Marcelino <cello.marcelino2735@gmail.com>
2025-05-16 12:45:03 +0700

    role validation(temp) in product_page

--- resources/views/pages/product_page.blade.php
+++ resources/views/pages/product_page.blade.php
@@ -12,6 +12,10 @@
     Kelas: IF 2A Malam
 -->
 
+@php
+    $user_role = 'costumer';
+@endphp
+
 @extends('layouts.app')
 @section('title', 'catalog')
 @section('content')
@@ -38,9 +42,11 @@
                 @include('components.catalogcard', ['productId' => $i])
                 @include('components.review_modal')
             @endfor 
-            <div data-modal-target="new-catalog" data-modal-toggle="new-catalog" class="flex border border-gray-500 rounded overflow-hidden justify-center items-center cursor-pointer">
+            @if ($user_role == 'admin')
+                 <div data-modal-target="new-catalog" data-modal-toggle="new-catalog" class="flex border border-gray-500 rounded overflow-hidden justify-center items-center cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"stroke-linejoin="round" class="feather feather-plus-square size-20 stroke-gray-500"><rect x="3" y="3" width="18"height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8"y1="12" x2="16" y2="12"></line></svg> 
             </div>
+            @endif
 
         </div>
     </div>
