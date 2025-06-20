006577fca5040a83232a316498a7bc1f3989cbb1
Ndan00 <hamdanthedk@gmail.com>
2025-06-20 20:37:52 +0700

    tambah Password Visibility

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -47,28 +47,62 @@ nim: 3312411001 -->
                 >
             </div>
 
-            <div class="mb-4">
-                <input 
-                    type="password" 
-                    name="password" 
-                    id="password" 
-                    required 
-                    placeholder="Password"
-                    class="w-full p-3 bg-gray-100 border border-transparent rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#DFAC6B]"
-                >
-            </div>
+                <div class="mb-4">
+                    <div class="relative">
+                        <input 
+                            type="password" 
+                            id="password"
+                            name="password"
+                            required
+                            placeholder="password"
+                            class="w-full p-3 pr-12 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-100 focus:ring-[#DFAC6B] focus:border-[#DFAC6B]"
+                        >
+                        <button type="button"
+                            onclick="togglePassword('password', this)"
+                            class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600">
+                            <svg id="eye-password" xmlns="http://www.w3.org/2000/svg" 
+                                class="h-5 w-5" fill="none" viewBox="0 0 24 24" 
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" 
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" 
+                                    d="M2.458 12C3.732 7.943 7.522 5 12 5c4.478 
+                                    0 8.268 2.943 9.542 7-1.274 4.057-5.064 
+                                    7-9.542 7-4.478 0-8.268-2.943-9.542-7z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
 
-            <div class="mb-6">
-                <input 
-                    type="password" 
-                    name="password_confirmation" 
-                    id="password_confirmation" 
-                    required 
-                    placeholder="Konfirmasi password"
-                    class="w-full p-3 bg-gray-100 border border-transparent rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#DFAC6B]"
-                >
-            </div>
+                <div class="mb-6">
+                    <div class="relative">
+                        <input 
+                            type="password" 
+                            id="password_confirmation"
+                            name="password_confirmation"
+                            required
+                            placeholder="konfirmasi password"
+                            class="w-full p-3 pr-12 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-100 focus:ring-[#DFAC6B] focus:border-[#DFAC6B]"
+                        >
+                        <button type="button"
+                            onclick="togglePassword('password_confirmation', this)"
+                            class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600">
+                            <svg id="eye-password_confirmation" xmlns="http://www.w3.org/2000/svg" 
+                                class="h-5 w-5" fill="none" viewBox="0 0 24 24" 
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" 
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" 
+                                    d="M2.458 12C3.732 7.943 7.522 5 12 5c4.478 
+                                    0 8.268 2.943 9.542 7-1.274 4.057-5.064 
+                                    7-9.542 7-4.478 0-8.268-2.943-9.542-7z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
 
+
+            
             <div class="flex justify-center">
                 <button type="submit" class="text-white bg-[#4B372F] hover:bg-[#3a2b25] focus:ring-10 focus:outline-none focus:ring-[#DFAC6B] shadow-lg shadow-[#4B372F]/50 font-large rounded-lg text-sm px-15 py-3 text-center mb-2">
                     Buat
@@ -82,3 +116,30 @@ nim: 3312411001 -->
     </div>
 </div>
 @endsection
+<script>
+    function togglePassword(fieldId, button) {
+        const input = document.getElementById(fieldId);
+        const icon = button.querySelector('svg');
+
+        if (input.type === "password") {
+            input.type = "text";
+            icon.innerHTML = `
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0 
+                    -8.268-2.943-9.542-7a10.05 10.05 0 
+                    012.109-3.378M9.88 9.88A3 3 0 
+                    0114.12 14.12M6.1 6.1l11.8 11.8" />
+            `;
+        } else {
+            input.type = "password";
+            icon.innerHTML = `
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                    d="M2.458 12C3.732 7.943 7.522 5 12 5c4.478 
+                    0 8.268 2.943 9.542 7-1.274 4.057-5.064 
+                    7-9.542 7-4.478 0-8.268-2.943-9.542-7z" />
+            `;
+        }
+    }
+</script>
