247c1e3f8d1a0b9d769da7b4d2207aa70a53e209
Deaasnuari <deaasnuari@gmail.com>
2025-07-02 00:09:25 +0700

    update lagiiii

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -9,14 +9,7 @@
 @section('content')
 
 <!-- Button kecil fixed di kiri atas -->
-<div class="max-w-5xl mx-auto px-6 md:px-12 py-4">
-    <a href="/about_us_admin"
-        class="inline-block bg-amber-600 hover:bg-amber-700 text-white text-xs font-semibold py-2 px-3 rounded shadow-md transition duration-300"
-       class="inline-block bg-amber-600 hover:bg-amber-700 text-white text-xs font-semibold py-2 px-3 rounded shadow-md transition duration-300"
-    >
-        ← About Us Admin
-    </a>
-</div>
+<!--   -->
 
     <!-- Main Section -->
     <div class="max-w-5xl mx-auto px-6 md:px-12 py-8 space-y-16">
